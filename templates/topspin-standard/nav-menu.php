9071eeff74aca1e18c6137bca7be228381dd57f7
Ethan Miller <ethanzanemiller@gmail.com>
2011-10-05 16:41:02 +0200

    Updated nav-menu.php to use ordered stores list to form menu.

--- templates/topspin-standard/nav-menu.php
+++ templates/topspin-standard/nav-menu.php
@@ -3,6 +3,7 @@
 
 global $store;
 $menu = $store->getSetting('topspin_navmenu');
+$storesListInOrder = $store->getStoresInOrder();
 
 ?>
 
@@ -10,7 +11,7 @@ $menu = $store->getSetting('topspin_navmenu');
 <?php if ($menu == 'on') : ?>
 <div class="topspin-store-navmenu">
 <ul class="topspin-store-navmenu">
-<?php foreach ($storesList as $item) : ?>
+<?php foreach ($storesListInOrder as $item) : ?>
   <li class="topspin-store-navmenu"><a class="topspin-store-navmenu" href="<?php bloginfo('wpurl'); ?>/<?php echo $item->post_name; ?>/"><?php echo $item->post_title; ?></a></li>
 <?php endforeach; ?>
 </ul>
